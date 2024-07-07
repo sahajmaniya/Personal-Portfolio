f055626679fd5ebf0f0c85cbb8a3b7bcce2cd352
sahajmaniya <sahajmaniya111@gmail.com>
2024-07-07 18:39:05 +0530

    commit

--- api/server.js
+++ api/server.js
@@ -25,7 +25,7 @@ contactEmail.verify((error) => {
   }
 });
 
-app.post("/api/contact", (req, res) => {
+app.post("/api/server", (req, res) => {
   const name = req.body.firstName + " " + req.body.lastName;
   const email = req.body.email;
   const message = req.body.message;
