1b9c60e523812537f61e20e2ab9931e41d13e276
sahajmaniya <sahajmaniya111@gmail.com>
2024-07-07 18:41:52 +0530

    commit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -28,7 +28,7 @@ export const Contact = () => {
     e.preventDefault();
     setButtonText('Sending...');
     try {
-      let response = await fetch('https://sahaj-maniya-portfolio.vercel.app/api/server', {
+      let response = await fetch('https://sahaj-maniya-portfolio.vercel.app/src/components/Contact.js', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json;charset=utf-8'
