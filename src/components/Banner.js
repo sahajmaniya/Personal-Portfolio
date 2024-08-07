d60691a76623b3349e1782d4a32935ce3b1b69dc
sahajmaniya <sahajmaniya111@gmail.com>
2024-08-07 19:40:10 +0530

    second commit

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Container, Row, Col} from "react-bootstrap";
-import headerImg from "../assets/img/profile-img.jpeg";
+import headerImg from "../assets/img/profile2.jpeg";
 import { ArrowRightCircle } from 'react-bootstrap-icons';
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
