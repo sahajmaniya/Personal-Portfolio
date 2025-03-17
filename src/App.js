2a01b34a60ebca9b321cb89480f0b2484126c057
sahajmaniya <sahajmaniya1120@gmail.com>
2025-03-17 10:15:24 +0530

    Added vercel insights dependency

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { Projects } from "./components/Projects";
 import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
 import HackathonSection from './components/HackathonSection';
-
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 
 function App() {
